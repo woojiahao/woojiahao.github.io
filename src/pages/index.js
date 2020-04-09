7b27bd81fb9052587e2fd55147c81eb4fdcb133d
woojiahao <woojiahao1234@gmail.com>
2020-04-09 15:46:15 +0800

    Fix content for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import {Link} from "gatsby"
 export default () => (
   <div className={indexStyles.homeContainer}>
     <div className={indexStyles.mainContainer}>
-      <h1>Hi! I am Woo Jia Hao!</h1>
+      <h1>Hi! 👋 I am Woo Jia Hao!</h1>
       <p>
         I am a software developer from the tiny red dot - Singapore! I recently graduated from Singapore Polytechnic
-        with a diploma in Information Technology. I am actively learning and <Link to="/blog">sharing about my journey
-        in programming.</Link> I am currently working on <Link to="/projects"> several projects</Link> and learning
+        with a Diploma in Information Technology. I am actively learning and <Link to="/blog">sharing about my
+        journey
+        in programming.</Link> I am currently working on <Link to="/projects"> several projects</Link> and
+        learning
         various programming languages to diversify my knowledge such as <Link to="/torrent-go">implementing the
         BitTorrent protocol using Go</Link> or <Link to="woojiahao-github-io">learning Gatsby.js to build this
         site.</Link>
