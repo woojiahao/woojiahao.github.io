978e47e4a8e3d7344d7cdae75a9b59c20da5d5cf
woojiahao <woojiahao1234@gmail.com>
2020-04-09 15:25:57 +0800

    Add more content for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import indexStyles from "./index.module.css"
+import {Link} from "gatsby"
 
 export default () => (
   <div className={indexStyles.homeContainer}>
@@ -7,20 +8,28 @@ export default () => (
       <h1>Hi! I am Woo Jia Hao!</h1>
       <p>
         I am a software developer from the tiny red dot - Singapore! I recently graduated from Singapore Polytechnic
-        with a diploma in Information Technology. I am actively looking to learn more about the world of software and
-        programming. I have been programming for five years already! I am currently working on several projects and
-        learning various programming languages to diversify my knowledge such as implementing the BitTorrent protocol
-        using Go or learning Gatsby.js to build this site.
+        with a diploma in Information Technology. I am actively learning and <Link to="/blog">sharing about my journey
+        in programming.</Link> I am currently working on <Link to="/projects"> several projects</Link> and learning
+        various programming languages to diversify my knowledge such as <Link to="/torrent-go">implementing the
+        BitTorrent protocol using Go</Link> or <Link to="woojiahao-github-io">learning Gatsby.js to build this
+        site.</Link>
       </p>
+
+      <p>
+        When I am not programming, I am learning about <Link to="/recommendations">various topics</Link> across
+        different domains! I also love to run and exercise.
+      </p>
+
       <div className={indexStyles.links}>
+        <strong>Check out my work!</strong>
         <p>Projects</p>
         <p>Blog</p>
+        <p>Recommendations</p>
         <p>About Me</p>
       </div>
-    </div>
 
-    <div className={indexStyles.contactContainer}>
-      <div>
+      <div className={indexStyles.contact}>
+        <strong>Stalk me!</strong>
         <p>GitHub</p>
         <p>Discord</p>
         <p>LinkedIn</p>
