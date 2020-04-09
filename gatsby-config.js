80891d616bf40f6c762bb2e3b1185c3fa7b7eaf4
woojiahao <woojiahao1234@gmail.com>
2020-04-09 20:39:05 +0800

    Add JSON transformer to load project details

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,23 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       }
     },
-    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-json`,
+      options: {
+        path: `./src/posts/projects/`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        path: `./src/posts/blog-posts/`
       }
     }
   ]
