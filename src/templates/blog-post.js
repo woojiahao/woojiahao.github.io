8623c15e599d83115277af3238069d68c243cefa
woojiahao <woojiahao1234@gmail.com>
2020-04-10 10:13:18 +0800

    Add date to each blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
 import {graphql} from "gatsby"
+import {getTitle} from "../utils/general"
+import blogPostStyles from "./blog-post.module.css"
 
 export default ({data}) => {
   const post = data.markdownRemark
+  const title = getTitle(post.fields.slug, post.frontmatter.title)
   return (
     <Layout>
       <div>
-        <h1>{post.frontmatter.title}</h1>
-        <h2>{post.frontmatter.date}</h2>
+        <h1>{title}</h1>
+        <h4 className={blogPostStyles.subtitle}>Published on: {post.frontmatter.date}</h4>
         <div dangerouslySetInnerHTML={{__html: post.html}}/>
       </div>
     </Layout>
@@ -21,7 +24,10 @@ export const query = graphql`
       html
       frontmatter {
         title
-        date
+        date(formatString: "DD MMMM YYYY")
+      }
+      fields {
+        slug
       }
     }
   }
