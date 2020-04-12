866306b7affda13ffebb796f0a2d35d6456a65fa
woojiahao <woojiahao1234@gmail.com>
2020-04-12 12:26:05 +0800

    Remove PageTitle component

--- src/components/page-title.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import style from "./page-title.module.css"
-
-export default props => (
-  <div className={style.title}>
-    <h1>{props.title}</h1>
-    {!props.hidePagination && <h3>Page {props.currentPage}/{props.numPages}</h3>}
-  </div>
-)
