799e4546129f68e36afe1ec8a2453b20a37468ce
woojiahao <woojiahao1234@gmail.com>
2020-04-12 11:52:34 +0800

    Add basic styling for project list

--- src/templates/project-list.js
+++ src/templates/project-list.js
@@ -6,25 +6,42 @@ import {getTitle} from "../utils/general"
 import PostPagination from "../components/post-pagination"
 
 export default ({data, pageContext}) => {
-  const posts = data.allMarkdownRemark.edges
+  const posts = data.allProjectsJson.edges
   return (
     <Layout>
-      {posts.map(({node: post}) => {
-        const title = getTitle(post.fields.slug, post.frontmatter.title)
+      <div className={projectStyles.generalTitle}>
+        <h1>My Projects</h1>
+        <h3>Page {pageContext.currentPage}/{pageContext.numPages}</h3>
+      </div>
 
-        const ttr = post.timeToRead
-        const timeToReadText = post.timeToRead <= 1 ? `${ttr} minute` : `${ttr} minutes`
+      {posts.map(({node: post}) => {
+        const title = getTitle(post.fields.slug, post.title)
+        let statusColor = "#ffaaa5"
+        switch (post.status) {
+          case "Completed":
+            statusColor = "a8e6cf"
+            break
+          case "In Progress":
+            statusColor = "ffd3b6"
+            break
+          case "Archived":
+            statusColor = "dcedc1"
+            break
+        }
 
         return (
-          <div className={projectStyles.blogCard}>
-            <Link to={post.fields.slug}>
-              <h1 className={projectStyles.title}>{title}</h1>
-            </Link>
-            <div className={projectStyles.subtitle}>
-              <h2>{post.frontmatter.date}</h2>
-              <h2 className={projectStyles.timeToRead}>{timeToReadText}</h2>
+          <div>
+            <div className={projectStyles.projectTitle}>
+              <Link to={post.fields.slug}>
+                <h3>{title}</h3>
+              </Link>
+              <div>
+                {post.technologies.languages.map(l => <span className={projectStyles.language}>{l}</span>)}
+                <span style={{backgroundColor: `#${statusColor}`}} className={projectStyles.status}>{post.status}</span>
+              </div>
             </div>
-            <p className={projectStyles.excerpt}>{post.excerpt}</p>
+
+            <p>{post.description}</p>
           </div>
         )
       })}
@@ -36,20 +53,19 @@ export default ({data, pageContext}) => {
 export const query = graphql`
   query ($limit: Int!, $skip: Int!) {
     allProjectsJson(
-      filter: { frontmatter: { published: { eq: true } } },
-      sort: { fields: [frontmatter___date], order: DESC },
+      filter: {published: {eq: true}},
+      sort: {fields: [duration___start], order: DESC},
       limit: $limit,
       skip: $skip
     ) {
       edges {
         node {
-          excerpt
-          timeToRead
-          frontmatter {
-            title
-            date(formatString: "DD MMMM YYYY")
-          }
+          title
           fields { slug }
+          technologies { languages }
+          status
+          description
+          images
         }
       }
     }
