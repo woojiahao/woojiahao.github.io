b655c104d425909b9a8f35082cf547550475ad48
woojiahao <woojiahao1234@gmail.com>
2020-04-12 12:13:45 +0800

    Add about and recommendations pages

---
+++ src/pages/recommendations.js
@@ -0,0 +1,9 @@
+import React from "react"
+import PageTitle from "../components/page-title"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <PageTitle hidePagination title="Recommendations"/>
+  </Layout>
+)
