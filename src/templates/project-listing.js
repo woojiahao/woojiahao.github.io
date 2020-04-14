90860e38e95a15f4394032b09c9d660cf1a0aecd
woojiahao <woojiahao1234@gmail.com>
2020-04-14 11:32:39 +0800

    Use two-col layout for project listing

--- src/templates/project-listing.js
+++ src/templates/project-listing.js
@@ -4,6 +4,7 @@ import {getTitle} from "../utils/general"
 import Post from "../classes/post"
 import style from "./project-listing.module.css"
 import ImageCarousel from "../components/image-carousel"
+import BigButton from "../components/big-button"
 import PostNavigation from "../components/post-navigation"
 
 export default ({data, pageContext}) => {
@@ -20,37 +21,55 @@ export default ({data, pageContext}) => {
   const languageLabel = languages && languages.length > 1 ? "Languages" : "Language"
   const libraryLabel = libraries && libraries.length > 1 ? "Libraries" : "Library"
 
+  const links = project.links
+  const repository = links.repository
+  const documentation = links.documentation
+  const site = links.site
+
   return (
     <Layout title={title} hidePagination>
       <div className={style.listing}>
         {project.images && <ImageCarousel folder={project.images}/>}
 
         <div className={style.content}>
-          <h2>Technologies</h2>
-          {languages &&
-          <div>
-            <h3>{languageLabel}</h3>
+          <div className={style.mainContent}>
+            <h2>Description</h2>
+            <p>{project.description}</p>
+
+            <h2>Lessons</h2>
             <ul>
-              {languages.map(l => <li>{l}</li>)}
+              {project.lessons.map(l => <li>{l}</li>)}
             </ul>
           </div>
-          }
-          {libraries &&
-          <div>
-            <h3>{libraryLabel}</h3>
-            <ul>
-              {libraries.map(l => <li>{l}</li>)}
-            </ul>
+
+          <div className={style.sidebar}>
+            <h2>Technologies</h2>
+
+            {languages &&
+            <div>
+              <h3>{languageLabel}</h3>
+              <ul>
+                {languages.map(l => <li>{l}</li>)}
+              </ul>
+            </div>
+            }
+
+            {libraries &&
+            <div>
+              <h3>{libraryLabel}</h3>
+              <ul>
+                {libraries.map(l => <li>{l}</li>)}
+              </ul>
+            </div>
+            }
+
+            <div className={style.links}>
+              {repository && <BigButton to={repository} bg="#6c567b" fg="white">Repository</BigButton>}
+              {documentation && <BigButton to={documentation} bg="#00bcd4" fg="white">Documentation</BigButton>}
+              {site && <BigButton to={site} bg="#f67280" fg="white">Site</BigButton>}
+            </div>
+
           </div>
-          }
-
-          <h2>Description</h2>
-          <p>{project.description}</p>
-
-          <h2>Lessons</h2>
-          <ul>
-            {project.lessons.map(l => <li>{l}</li>)}
-          </ul>
         </div>
       </div>
 
@@ -61,39 +80,44 @@ export default ({data, pageContext}) => {
 
 // TODO Add more fields in the future like end and libraries
 export const query = graphql`
-        query (
-        $slug: String!,
-        $prev: String,
-        $next: String
-        ) {
-        projectsJson(
-        fields: {slug: {eq: $slug}}
-        ) {
-        title
-        images
-        lessons
-        description
-        status
-        duration {
+  query (
+    $slug: String!,
+    $prev: String,
+    $next: String
+  ) {
+    projectsJson(
+      fields: {slug: {eq: $slug}}
+    ) {
+      title
+      images
+      lessons
+      description
+      status
+      duration {
         start
         end
-        }
-        technologies {
+      }
+      technologies {
         languages
         libraries
-        }
-        fields { slug }
-        }
-        allProjectsJson(
-        filter: {fields: {slug: {in: [$next, $prev]}}}
-        ) {
-        edges {
+      }
+      links {
+        documentation
+        repository
+        site
+      }
+      fields { slug }
+    }
+    allProjectsJson(
+      filter: {fields: {slug: {in: [$next, $prev]}}}
+    ) {
+      edges {
         node {
-        fields { slug }
-        published
-        title
+          fields { slug }
+          published
+          title
         }
-        }
-        }
-        }
-        `
+      }
+    }
+  }
+`
