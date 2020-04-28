3d52533abdb59a8c98859940da8f5f98ce2986ca
woojiahao <woojiahao1234@gmail.com>
2020-04-28 15:35:51 +0800

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout tabTitle="Not found" pageTitle="Page not found">
+    <p>The page you have requested is not found!</p>
+  </Layout>
+)
