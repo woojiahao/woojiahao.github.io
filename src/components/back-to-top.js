40c6fb26f9ece00eed8e53965d72518c7275379a
woojiahao <woojiahao1234@gmail.com>
2020-04-28 15:55:19 +0800

    Fix 'window is not defined' error

--- src/components/back-to-top.js
+++ src/components/back-to-top.js
@@ -3,12 +3,14 @@ import {AiOutlineArrowUp} from "react-icons/all"
 import style from "./back-to-top.module.css"
 
 const BackToTop = () => {
-  window.addEventListener('scroll', () => {
-    const arrow = document.querySelector(`div.${style.arrow}`)
-    if (arrow) {
-      window.scrollY ? arrow.classList.remove(style.hidden) : arrow.classList.add(style.hidden)
-    }
-  })
+  if (typeof window !== `undefined`) {
+    window.addEventListener('scroll', () => {
+      const arrow = document.querySelector(`div.${style.arrow}`)
+      if (arrow) {
+        window.scrollY ? arrow.classList.remove(style.hidden) : arrow.classList.add(style.hidden)
+      }
+    })
+  }
 
   const onClick = () => {
     document.body.scrollTop  = 0
