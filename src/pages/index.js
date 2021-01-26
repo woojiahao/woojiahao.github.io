be885f5908a3c2a737fc11ea35f6780bde59633a
woojiahao <woojiahao1234@gmail.com>
2021-01-26 22:50:22 +0800

    Update styles for homepage to highlight quick link

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,14 +57,12 @@ const Home = ({ data }) => {
             <h1>Hi!<span role="img" aria-label="wave"> 👋 </span>I am Jia Hao!</h1>
             <p>
               I am a software developer from the tiny red dot - Singapore! I go by the moniker of Chill online.
-              I graduated from Singapore Polytechnic with a Diploma in Information Technology and will be pursuing
-              a degree in Computer Science at the National University of Singapore after I complete my national service!
+              I graduated from Singapore Polytechnic with a Diploma in Information Technology!
           </p>
 
             <p>
               I have performed natural language processing research <Link to="/about"> @ DSO National Laboratories,</Link> managed
-            UAT across 90+ teams <Link to="/about"> @ DBS Bank,</Link> and pioneered a usability framework
-            <Link to="/about"> @ MightyAim!</Link>
+            UAT across 90+ teams <Link to="/about"> @ DBS Bank,</Link> and pioneered a usability framework <Link to="/about">@ MightyAim!</Link>
             </p>
 
             <p>
@@ -78,29 +76,35 @@ const Home = ({ data }) => {
           <div className={style.recent}>
             <h1 className={style.subtitle}>I love...</h1>
 
-            <h2>Sharing about programming</h2>
-            <QuickLink
-              title={latestBlogPost.frontmatter.title}
-              description={latestBlogPost.frontmatter.date}
-              link={latestBlogPost.fields.slug} />
-            <Link to="/blog" className={style.viewAll}>View other posts</Link>
+            <div className={style.recentBox}>
+              <h2>Sharing about programming:</h2>
+              <QuickLink
+                title={latestBlogPost.frontmatter.title}
+                description={latestBlogPost.frontmatter.date}
+                link={latestBlogPost.fields.slug} />
+              <Link to="/blog" className={style.viewAll}>View other posts</Link>
+            </div>
 
-            <h2>Building projects</h2>
-            <QuickLink
-              title={latestProject.title}
-              description={latestProject.description}
-              link={latestProject.fields.slug} />
-            <Link to="/projects" className={style.viewAll}>View other projects</Link>
+            <div className={style.recentBox}>
+              <h2>Building projects:</h2>
+              <QuickLink
+                title={latestProject.title}
+                description={latestProject.description}
+                link={latestProject.fields.slug} />
+              <Link to="/projects" className={style.viewAll}>View other projects</Link>
+            </div>
 
-            <h2>Teaching</h2>
-            <QuickLink
-              title="Git Guide"
-              description="A practical introduction to the Git version control system. Written for SP DIT SEP module."
-              link="https://woojiahao.github.io/git-guide" />
-            <QuickLink
-              title="Introduction to Android Development with Kotlin"
-              description="Learn the basics of Android development using Kotlin by building a basic to-do application."
-              link="https://woojiahao.github.io/KotlinToDo" />
+            <div className={style.recentBox}>
+              <h2>Teaching:</h2>
+              <QuickLink
+                title="Git Guide"
+                description="A practical introduction to the Git version control system. Written for SP DIT SEP module."
+                link="https://woojiahao.github.io/git-guide" />
+              <QuickLink
+                title="Introduction to Android Development with Kotlin"
+                description="Learn the basics of Android development using Kotlin by building a basic to-do application."
+                link="https://woojiahao.github.io/KotlinToDo" />
+            </div>
 
           </div>
         </main>
