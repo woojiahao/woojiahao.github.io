f5ce1f6c35ace5db3c46f322d81e554dc3edc427
woojiahao <woojiahao1234@gmail.com>
2021-01-29 11:30:22 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4e698464219f6e707651.js"
+    "url": "webpack-runtime-00b61d8495cb68473bfc.js"
   },
   {
-    "url": "styles.b1739a07432d52dc1f0f.css"
+    "url": "styles.d6e1ffcfa7d062512d9b.css"
   },
   {
     "url": "styles-e9d24b1846c7d6eb9685.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "10060b31797fbdb885b84e03122730d1"
+    "revision": "1d5b363605a6afcfdf648e6c40ca7b69"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-780d0321bb4481125aff.js"
